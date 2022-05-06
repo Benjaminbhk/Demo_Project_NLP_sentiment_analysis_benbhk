3a50c4aa2be1819650a078b41fdf1d78cad43152
benjamin barre <benjamin.barre@gmail.com>
2022-05-06 15:56:37 +0800

    some inspiration (twitter account link)

--- Demo_Project_NLP_sentiment_analysis_benbhk/app.py
+++ Demo_Project_NLP_sentiment_analysis_benbhk/app.py
@@ -79,6 +79,15 @@ if sentence != '':
     # -------------------------------------------------------------------------------------------------#
     # Text imput and processing
 
+    st.markdown(
+        '''If you are looking for some inspiration, here are some links to twitter accounts..
+                - [Barack Obama](https://twitter.com/BarackObama?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor)
+                - [Elon Musk](https://twitter.com/elonmusk?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor)
+                - [CNN](https://twitter.com/CNN?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor)
+                - [Cristiano Ronaldo](https://twitter.com/Cristiano?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor)
+                - [The New York Times](https://twitter.com/nytimes?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor)
+                ''')
+
     sentence = str.encode(sentence)
     sentence = text_to_word_sequence(sentence.decode("utf-8"))
     sentence= [sentence]
